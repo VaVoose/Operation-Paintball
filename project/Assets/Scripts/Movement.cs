329dbb4319d1338195f44937c033b9fe54260203
vavoose <daf5-31@sbcglobal.net>
2016-11-17 19:38:00 -0500

    fucked things up

--- project/Assets/Scripts/Movement.cs
+++ project/Assets/Scripts/Movement.cs
@@ -4,25 +4,49 @@ using System.Collections.Generic;
 
 
 public class Movement : MonoBehaviour {
-	public int maxSpeed = 5;
-	public float currentSpeed = 0.0f;
-	public float acceleration = .03f;
-	public float decceleration = .01f;
-	public float startAccel = 0.0f;
+	public float speed = 6;
+	public float currentVel = 1;
+	public float accel = 0.2f;
+	public float deccel = 1f;
 
-	void FixedUpdate(){
-        if (currentSpeed != 0 && Input.GetKeyDown(KeyCode.W) && Input.GetKeyDown(KeyCode.S))
+
+	void Update(){
+        if (Input.GetKey(KeyCode.W)) {
+            Step("w");
+        }
+        if (Input.GetKey(KeyCode.S))
         {
-            currentSpeed = Mathf.Lerp(currentSpeed, startAccel, Time.time * decceleration);
-            acceleration = 0;
+            Step("s");
         }
-        else {
-            if (currentSpeed < Max)
+        if (Input.GetKey(KeyCode.D))
+        {
+            Step("d");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Step("a");
         }
 
 
-		 
-	}
+    }
+
+    public void Step(string direction) {
+        switch (direction) {
+            case "w":
+                transform.Translate((Vector3.forward * Time.deltaTime)*speed);
+                break;
+            case "s":
+                transform.Translate((Vector3.back * Time.deltaTime)*speed);
+                break;
+            case "a":
+                transform.Translate((Vector3.left * Time.deltaTime)*speed);
+                break;
+            case "d":
+                transform.Translate((Vector3.right * Time.deltaTime)*speed);
+                break;
+
+        }
+    }
 
 
 
