866971274c03d0dc4b8fb40c182c8d780b67934b
Ferrante <17DoFerrante@MFCSD.ORG>
2016-11-21 14:57:49 -0500

    added a paintball shooting script

---
+++ project/Assets/Scripts/Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	public GameObject paintballPrefab;
+
+	void Update(){
+		GameObject goPaintball;
+
+
+		if (Input.GetMouseButtonDown (0)) {
+			goPaintball = Instantiate (paintballPrefab);
+			goPaintball.GetComponent<MeshRenderer> ().material.color = Random.ColorHSV();
+			goPaintball.transform.position = this.gameObject.transform.position;
+			goPaintball.transform.rotation = this.gameObject.transform.rotation;
+			goPaintball.GetComponent<Rigidbody> ().AddRelativeForce (new Vector3 (0, 0, 5000));
+		}
+	}
+		
+}
